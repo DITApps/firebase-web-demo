f73ac7c94ba4a846ec4d4f47085423a12f88a5bb
Bayrem <garssallaoui.bayrem@gmail.com>
2019-06-30 02:16:06 -0500

    heartbeat

--- public/app.js
+++ public/app.js
@@ -9,6 +9,7 @@
 	const tempElement = document.getElementById('temperature');
 	const humElement = document.getElementById('humidity');
 	const gasElement = document.getElementById('gas');
+	const heartbeatElement = document.getElementById('status');
 	// Create temperature database reference
 	const tempRef = database.ref('dht11').child('temperature');
 
@@ -17,6 +18,9 @@
 
 	// Create gas database reference
 	const gasRef = database.ref('mq5').child('gasStatus');
+
+	// Create heartbeat database reference
+	const heartRef = database.ref('status').child('heartbeat');
 
 	// Sync objects changes
 	tempRef.limitToLast(1).on('value', function(snapshot) {
@@ -38,6 +42,13 @@
 			var childData = childSnapshot.val();
 			console.log("gas: " + childData);
 			gasElement.innerText = childData;
+		});
+	});
+	heartRef.limitToLast(1).on('value', function(snapshot) {
+		snapshot.forEach(function(childSnapshot) {
+			var childData = childSnapshot.val();
+			console.log("heartbeat: " + childData);
+			heartbeatElement.innerText = childData;
 		});
 	});
 
