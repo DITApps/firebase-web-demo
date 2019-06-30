dcd227d5223567d12ba0ea773207034df46fd12b
kaizoku-619 <garssallaoui.bayrem@gmail.com>
2019-06-30 00:06:27 -0500

    first

---
+++ public/app.js
@@ -0,0 +1,44 @@
+(function() {
+
+	firebase.initializeApp(config);
+
+	// Get a reference to the database service
+	var database = firebase.database();
+
+	// Get element from the DOM
+	const tempElement = document.getElementById('temperature');
+	const humElement = document.getElementById('humidity');
+	const gasElement = document.getElementById('gas');
+	// Create temperature database reference
+	const tempRef = database.ref('dht11').child('temperature');
+
+	// Create humidity database reference
+	const humRef = database.ref('dht11').child('humidity');
+
+	// Create gas database reference
+	const gasRef = database.ref('mq5').child('gasStatus');
+
+	// Sync objects changes
+	tempRef.limitToLast(1).on('value', function(snapshot) {
+		snapshot.forEach(function(childSnapshot) {
+			var childData = childSnapshot.val();
+			console.log("temperature: " + childData);
+			tempElement.innerText = childData;
+		});
+	});
+	humRef.limitToLast(1).on('value', function(snapshot) {
+		snapshot.forEach(function(childSnapshot) {
+			var childData = childSnapshot.val();
+			console.log("humidity: " + childData);
+			humElement.innerText = childData;
+		});
+	});
+	gasRef.limitToLast(1).on('value', function(snapshot) {
+		snapshot.forEach(function(childSnapshot) {
+			var childData = childSnapshot.val();
+			console.log("gas: " + childData);
+			gasElement.innerText = childData;
+		});
+	});
+
+}());
---
+++ public/config.js
@@ -0,0 +1,10 @@
+// Set the configuration for your app
+var config = {
+  apiKey: "AIzaSyCAQ4BzK5ubksCpo26SSVBewVhtAuYTv2A",
+  authDomain: "medium-iot-demo.firebaseapp.com",
+  databaseURL: "https://medium-iot-demo.firebaseio.com",
+  projectId: "medium-iot-demo",
+  storageBucket: "",
+  messagingSenderId: "315417917583",
+  appId: "1:315417917583:web:de22690bf77dd3f6"
+};
